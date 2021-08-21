f64a8beab81bb76a6db1859a5f46e65b14d346eb
Minja Popovic <mini.me@mein.gmx>
2021-08-21 10:29:38 +0200

    Dropdown

---
+++ src/lib/components/Dropdowns/DropdownFilled.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DropdownFilled from './DropdownFilled'
+
+export default {
+	title: 'Example/Dropdown',
+	component: DropdownFilled,
+}
+
+const Template = (args) => <DropdownFilled {...args} />
+export const Filled = Template.bind({})
